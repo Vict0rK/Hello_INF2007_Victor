9114ed18060f4366e5a56d22f866c1897533eb75
Victor <113668641+Vict0rK@users.noreply.github.com>
2024-01-19 17:25:29 +0800

    update name

--- app/src/main/java/com/example/helloinf2007/MainActivity.kt
+++ app/src/main/java/com/example/helloinf2007/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
                     Column {
                         Greeting("INF2007!")
-                        From("Michael")
+                        From("Victor")
 
                     }
                 }
